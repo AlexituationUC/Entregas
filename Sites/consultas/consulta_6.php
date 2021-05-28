ead3348b66e31803413df8715daada44293b7807
Gonzalo-J <gonzalo.jimenez@uc.cl>
2021-05-28 18:44:56 -0400

    correcion 19

--- Sites/consultas/consulta_6.php
+++ Sites/consultas/consulta_6.php
@@ -15,21 +15,21 @@
     ORDER BY cantidad DESC;";
   }
   if ($producto == "Comestible Frescos") {
-    $query = "SELECT t.nombre, COUNT(c.id) as cantidad
+    $query = "SELECT t.nombre, COUNT(f.id) as cantidad
     FROM Frescos as f, Comestibles as c, Productos as p, carritos as ca, Compras as co, Tiendas as t
     WHERE f.id = c.id AND c.id = p.id AND p.id = ca.id_productos AND ca.id_compras = co.id AND co.id_tienda = t.id
     GROUP BY t.nombre
     ORDER BY cantidad DESC;";
   }
   if ($producto == "Comestible Conserva") {
-    $query = "SELECT t.nombre, COUNT(c.id) as cantidad
+    $query = "SELECT t.nombre, COUNT(cons.id) as cantidad
     FROM Conserva as cons, Comestibles as c, Productos as p, carritos as ca, Compras as co, Tiendas as t
-    WHERE f.id = c.id AND c.id = p.id AND p.id = ca.id_productos AND ca.id_compras = co.id AND co.id_tienda = t.id
+    WHERE cons.id = c.id AND c.id = p.id AND p.id = ca.id_productos AND ca.id_compras = co.id AND co.id_tienda = t.id
     GROUP BY t.nombre
     ORDER BY cantidad DESC;";
   }
   if ($producto == "Comestible Congelado") {
-    $query = "SELECT t.nombre, COUNT(c.id) as cantidad
+    $query = "SELECT t.nombre, COUNT(cong.id) as cantidad
     FROM Congelados as cong, Comestibles as c, Productos as p, carritos as ca, Compras as co, Tiendas as t
     WHERE cong.id = c.id AND c.id = p.id AND p.id = ca.id_productos AND ca.id_compras = co.id AND co.id_tienda = t.id
     GROUP BY t.nombre
