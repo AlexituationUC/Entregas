2b7de2802dd92a15fb93d71ee2ded495fa6c3466
Gonzalo-J <gonzalo.jimenez@uc.cl>
2021-07-01 17:32:03 -0400

    registro e importacion

---
+++ Sites/procesos_almacenamiento_registro/asignar_contrasenas.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE FUNCTION
+
+-- declaramos la función
+asignar_contrasenas ()
+
+-- declaramos lo que retorna 
+RETURNS BOOLEAN AS $$
+
+-- declaramos la idmax de los usurios y de las direcciones
+DECLARE
+usuario record;
+
+-- definimos nuestra función
+BEGIN
+
+    -- verificar si existe la columna clave, si no existe la agregamos y seteamos todos los
+    -- valores como un espacio
+    IF 'clave' NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='info_Usuarios') THEN
+        ALTER TABLE info_Usuarios ADD clave varchar(255);
+        UPDATE info_Usuarios SET clave = " ";
+    END IF;
+
+    -- asignamos las claves a los usuarios por medio de un loop
+    FOR usuario in SELECT rut, clave FROM info_Usuarios
+    LOOP
+        -- las contraseñas corresponden a los primeros 4 digitos del rut
+        usuario.clave := SUBSTRING(usuario.rut, 1, 4)
+    END LOOP;
+
+    -- se retorna un TRUE para señalar que ya se asignaron las contraseñas
+    RETURN TRUE
+
+END
+$$ language plpgsql
