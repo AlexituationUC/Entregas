2021af8701591a0c0224816e390c7bd6d3c75836
Gabriel <gabriel.vega@uc.cl>
2021-07-05 10:23:13 -0400

    conexion :c

--- Sites/templates_php/convertir_usuarios.php
+++ Sites/templates_php/convertir_usuarios.php
@@ -1,6 +1,7 @@
 <?php
 
     // Obtenemos el personal de administracion de la base de datos del grupo par
+    require("../config/conexion.php");
     $query = "SELECT p.rut as rut, p.nombre as nombre, p.edad as edad, d.nombre_direccion as direccion
               FROM Personal as p, Administrativos as a, Unidades as u, Direcciones as d
               WHERE p.id = a.id AND a.id_unidades = u.id AND u.id_direcciones = d.id;";
