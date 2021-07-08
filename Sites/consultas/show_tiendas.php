8408db97bd54aa95b473fad8bcbaf7b0335f55ab
Gabriel <gabriel.vega@uc.cl>
2021-07-07 23:25:13 -0400

    map

--- Sites/consultas/show_tiendas.php
+++ Sites/consultas/show_tiendas.php
@@ -1,4 +1,55 @@
-<?php include('../templates_html/header.php'); ?>
+<?php
+
+if (!empty($_POST)) {
+  $id = $_POST["id"];
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title> DCCompras </title>
+    <!-- Bootstrap(CSS), Jquery (javascripts), etc... -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- para que sea index.php pueda importarlo -->
+    <link rel="stylesheet" href="styles/mystyles.css">
+    <!-- para que una consulta.php pueda importarlo -->
+    <link rel="stylesheet" href="../styles/mystyles.css">
+
+    <script src="https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js"></script>
+    <link href="https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css" rel="stylesheet"/>
+    
+</head>
+
+<div class="bkg">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index_logged.php">Home</a>
+    <?php if (!empty($_POST)){echo "<input type='hidden' name='id' value=$id class='form-control'>";} ?>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Info</a>
+        </li>
+      </ul>
+      <form align="center" action="perfil.php" method="post">
+        <?php if (!empty($_POST)){echo "<input type='hidden' name='id' value=$id class='form-control'>";} ?>
+        <button type="submit" class="btn btn-primary"> Perfil </button>
+      </form>
+    </div>
+  </div>
+</nav>
+<br><br>
 
 <body>
 
@@ -143,6 +194,38 @@
       <button type="submit" class="btn btn-primary"> Volver a lista de tiendas </button>
     </form>
 
+    <br><br><br>
+
+    <?php
+        $query_map = $db -> prepare("SELECT Direcciones.latitude, Direcciones.longitude
+                                             FROM Direcciones, Tiendas
+                                             WHERE Direcciones.id = Tiendas.id_direccion
+                                             AND Tiendas.id = $id_tienda;");
+        $query_map -> execute();
+        $direccion_map = $query_map -> fetchAll();
+        foreach ($direccion_map as $map_dir){
+          $latitude = $map_dir[0];
+          $longitude = $map_dir[1];
+        }
+    ?>
+    
+    <center> <div id='map' style='width: 400px; height: 300px;'></div>
+        <script>
+            mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FiMC12aCIsImEiOiJja3FmbjBhaG4wczdkMm9udXFxYWtydHl2In0.dNR8LX-3sxjCx6h7xG8Mng';
+            var map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [<?php echo $longitude ?>, <?php echo $latitude ?>], //lng,lat
+                zoom: 11
+            });
+            var marker = new mapboxgl.Marker()
+            .setLngLat([<?php echo $longitude ?>, <?php echo $latitude ?>])
+            .setPopup(new mapboxgl.Popup().setHTML("<h1> Pop up </h1>"))
+            .addTo(map);
+            map.addControl(new mapboxgl.NavigationControl());
+        </script>
+    </center>
+    
     <br><br><br><br>
   
   </div>
