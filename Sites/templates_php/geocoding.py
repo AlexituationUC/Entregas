d806538a940c8bdd8863f1a26cb4b98be33ea037
Gabriel <gabriel.vega@uc.cl>
2021-07-07 22:33:07 -0400

    A

--- Sites/templates_php/geocoding.py
+++ Sites/templates_php/geocoding.py
@@ -1,13 +1,24 @@
-from opencage.geocoder import OpenCageGeocode
 import sys
-
-ubicacion = sys.argv[1]
+from opencage.geocoder import OpenCageGeocode
+import csv
 
 key = 'e0449567eaaf40c68c95e7238d390381'
 geocoder = OpenCageGeocode(key)
 
-query = ubicacion
-results = geocoder.geocode(query)
+with open('Sites/templates_php/data_direcciones.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        address = row['Direccion'].strip()
+        id_dir = int(row['id'])
+        results = geocoder.geocode(address, no_annotations='1')
+        if results and len(results):
+            longitude = results[0]['geometry']['lng']
+            latitude = results[0]['geometry']['lat']
+            print(u'%i,%f;%f;%s' % (id_dir, latitude, longitude, address))
 
-print(results[0]['geometry']['lat'],
-      results[0]['geometry']['lng'])
+
+# query = ubicacion
+# results = geocoder.geocode(query)
+
+# print(results[0]['geometry']['lat'],
+#       results[0]['geometry']['lng'])
