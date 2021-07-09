98bc102f10a2dbf68d35bceda12d61d352e71731
AlexituationUC <alexis.aracena@uc.cl>
2021-07-08 22:47:13 -0400

    sdf

--- Sites/consultas/index_productos.php
+++ Sites/consultas/index_productos.php
@@ -8,12 +8,26 @@
   	$tipo_consulta = $_POST["tipo_consulta"];
   	$id_tienda = intval($_POST["id_tienda"]);
 
-	$query_comestible = "SELECT tres_mas_baratos('comestible', $id_tienda);";
+	$query_comestible = "SELECT Productos.nombre, Productos.precio, Productos.descripcion
+    FROM Productos, Comestibles, Tiendas, tienen
+    WHERE Productos.id = Comestibles.id
+    AND Tiendas.id = tienen.id_tiendas
+    AND Productos.id = tienen.id_productos
+    AND Tiendas.id = $id_tienda
+    ORDER BY Productos.precio
+    LIMIT 3";
 	$resultado_comestible = $db -> prepare($query_comestible);
 	$resultado_comestible -> execute();
 	$comestibles = $resultado_comestible -> fetchAll();
 
-	$query_no_comestible = "SELECT tres_mas_baratos('no_comestible', $id_tienda);";
+	$query_no_comestible = "SELECT Productos.nombre, Productos.precio, Productos.descripcion
+    FROM Productos, No_Comestibles, Tiendas, tienen
+    WHERE Productos.id = No_Comestibles.id
+    AND Tiendas.id = tienen.id_tiendas
+    AND Productos.id = tienen.id_productos
+    AND Tiendas.id = $id_tienda
+    ORDER BY Productos.precio
+    LIMIT 3;";
 	$resultado_no_comestible = $db -> prepare($query_no_comestible);
 	$resultado_no_comestible -> execute();
 	$no_comestibles = $resultado_no_comestible -> fetchAll();
@@ -37,9 +51,9 @@
 				echo $p[1];
 				$p = $p[0];
 				if ($p[0] == "") {
-					echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td> </td></tr>";
+					echo "<tr><td> $p[0] </td><td> $p[1] </td><td> $p[2] </td><td> $p[3] </td><td> </td></tr>";
 				} else {
-					echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>
+					echo "<tr><td> $p[0] </td><td> $p[1] </td><td> $p[2] </td><td> $p[3] </td><td>
 					<form align='center' action='show_productos.php' method='post'>
 					<div class='form-floating'>
 					<input type='hidden' name='id_tienda' value=$id_tienda class='form-control'>
