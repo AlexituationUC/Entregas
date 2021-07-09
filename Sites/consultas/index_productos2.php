b0c5bf3c710e86715ec1b66f5b54309b13e3f1b7
Gabriel <gabriel.vega@uc.cl>
2021-07-08 22:29:34 -0400

    limieza

--- Sites/consultas/index_productos2.php
+++ Sites/consultas/index_productos2.php
@@ -7,9 +7,13 @@
 
   	$tipo_consulta = $_POST["tipo_consulta"];
   	$id_tienda = intval($_POST["id_tienda"]);
-
     $producto = $_POST["producto"];
-    $query = "SELECT vendidos_por_tienda($id_tienda, $producto)";
+    $query = "SELECT Productos.id, Productos.nombre, Productos.descripcion, Productos.precio
+              FROM Productos, Tiendas, tienen
+              WHERE Productos.id = tienen.id_productos 
+              AND Tiendas.id = tienen.id_tiendas
+              AND LOWER(Productos.nombre) LIKE LOWER('%$producto%')
+              AND Tiendas.id = $id_tienda;";
     $resultado = $db -> prepare($query);
     $resultado -> execute();
     $productos = $resultado -> fetchAll();
